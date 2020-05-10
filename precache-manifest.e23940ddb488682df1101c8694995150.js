96f8809598be16190c185ec60ccb0086d8788637
martin chan <martinchan994@gmail.com>
2020-05-10 21:21:36 +0200

    Updates

---
+++ precache-manifest.e23940ddb488682df1101c8694995150.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195f1a01c813d56054ad1f2eacccd4b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f77cca64bd75f5a6ca09",
+    "url": "/static/css/2.96e8b77f.chunk.css"
+  },
+  {
+    "revision": "21d864a0737945f0d550",
+    "url": "/static/css/main.03d89ef8.chunk.css"
+  },
+  {
+    "revision": "f77cca64bd75f5a6ca09",
+    "url": "/static/js/2.2143c4a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2143c4a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21d864a0737945f0d550",
+    "url": "/static/js/main.3f610a9d.chunk.js"
+  },
+  {
+    "revision": "5bd8baa0c63da98153c6",
+    "url": "/static/js/runtime-main.c288d095.js"
+  }
+]);
